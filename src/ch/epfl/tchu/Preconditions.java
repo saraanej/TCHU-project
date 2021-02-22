386b5b9da5032f70b7ce5bb8a7eddb7a834f4b8f
yasmineben510 <yasmin.benrahhal@epfl.chh>
2021-02-22 15:53:00 +0100

    precon

--- src/ch/epfl/tchu/Preconditions.java
+++ src/ch/epfl/tchu/Preconditions.java
@@ -1,14 +1,33 @@
 package ch.epfl.tchu;
 
+/**
+ * class Preconditions - cannot be instanciated
+ * 
+ * 
+ * @author Yasmin Benrahhal
+ * @author Sara Anejjar
+ *
+ */
 public final class Preconditions {
 	
+	/**
+	 * private default constructor
+	 * 
+	 */
 	private Preconditions() {}
 	
+	/**
+	 * checks preconditions are satisfied 
+	 * 
+	 * @throws an IllegalArgumentException if the preconditions are not satisfied
+	 * 
+	 * @param shouldBeTrue (boolean) the precondition to satisfy
+	 * 
+	 */
 	public static void checkArgument (boolean shouldBeTrue) {
-		//!shouldBeTrue ? throw new IllegalArgumentException;
 		
 		if (!shouldBeTrue) {
-			throw new IllegalArgumentException;
+			throw new IllegalArgumentException();
 		}
 		
 	}
