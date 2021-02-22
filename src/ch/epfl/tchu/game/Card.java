0d805757466b3fc557937c82eee42a2cd8945892
yasmineben510 <yasmin.benrahhal@epfl.chh>
2021-02-23 00:32:52 +0100

    card

--- src/ch/epfl/tchu/game/Card.java
+++ src/ch/epfl/tchu/game/Card.java
@@ -1,17 +1,15 @@
 package ch.epfl.tchu.game;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * 
+ * Modelizes the different types of cards 
+ * used in the game
+ * 
  * @author Yasmin Benrahhal (329912)
  * @author Sara Anejjar (329905)
- * 
- * enum type represents the different types of cards 
- * used in the game
- *
  */
 
 public enum Card {
@@ -28,45 +26,50 @@ public enum Card {
 	
 	public final static List<Card> ALL = List.of(Card.values());
 	public final static int COUNT = ALL.size(); 
-	public final static List<Card> CARS = List.of(
+	
+	/**
+	 * List only containing the wagon cards
+	 */
+	public final static List<Card> CARS = Arrays.asList(
+
 	BLACK, VIOLET, BLUE, GREEN, YELLOW, ORANGE, RED, WHITE);
 
-	private Color colorName;
 	
-	private Color colorName() {
-		return colorName;
+	/**
+	 * 
+	 * @param color : the card's color.
+	 * @return the type of the corresponding wagon card depending on the card's color.
+	 */
+	
+	public static Card of(Color color) {
+		
+		switch(color) {
+		case BLACK : return Card.BLACK;
+		case VIOLET : return Card.VIOLET; 
+		case BLUE : return Card.BLUE;
+		case GREEN : return Card.GREEN;
+		case YELLOW : return Card.YELLOW;
+		case ORANGE : return Card.ORANGE;
+		case RED : return Card.RED;
+		case WHITE : return Card.WHITE;
+		default : return null;
+		}
+		
 	}
 	
 	/**
-	 * @return a new List<Card> containing only the wagon cards.
-	 */
-	
-	
-	
-	
-	
-	/**
 	 * Constructor initializing the color of the wagon cards. 
-	 * @param colorName
+	 * @param color
 	 */
 	private Card(Color color) {
 		colorName = color;
 	}
 	
+	private Color colorName;
 	
-	/**
-	 * 
-	 * @param color : the card's color.
-	 * @return the type of the corresponding wagon card.
-	 */
-	
-	
-	/*public static Card of(Color color) {
-		switch(colorName) {
-		
-		}
-		return ;
-	}*/
+	private Color colorName() {
+		return colorName;
+	}
 	
 	
 	/**
@@ -74,9 +77,11 @@ public enum Card {
 	 * @return the color of the card's type if it's a wagon card. null if not.
 	 */
 	public Color color() {
-		
 		return (this.colorName() == null) ?  null :  this.colorName();	
 	}
+	
+	
+	
 	
 /*private static List<Card> removed() {
 		
