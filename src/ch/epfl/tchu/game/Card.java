e49aea329c415376d19cf9e89d606696eafe0fb6
saraanej <sara.anejjar@epfl.ch>
2021-02-22 16:36:09 +0100

    Update Card.java

--- src/ch/epfl/tchu/game/Card.java
+++ src/ch/epfl/tchu/game/Card.java
@@ -11,29 +11,67 @@ import java.util.List;
 
 public enum Card {
 
-	BLACK,
-	VIOLET,
-	BLUE,
-	GREEN,
-	YELLOW,
-	ORANGE,
-	RED,
-	WHITE,
-	LOCOMOTIVE;
+	BLACK ("black"),
+	VIOLET ("violet"),
+	BLUE ("blue"),
+	GREEN ("green"),
+	YELLOW ("yellow"),
+	ORANGE ("orange"),
+	RED ("red"),
+	WHITE ("white"),
+	LOCOMOTIVE (null);
 	
 	public final static List<Card> ALL = List.of(Card.values());
 	public final static int COUNT = ALL.size(); 
-	static List<Card> c = List.of(Card.values());
-	public final static List<Card> CARS = c.remove(9);
+	public final static List<Card> CARS = remove();
 	
-	public static Card of(Color color) {
-		
-		
-		return color;
+	private  String colorName;
+	
+	private  String colorName() {
+		return colorName;
 	}
 	
+	/**
+	 * @return a new List<Card> containing only the wagon cards.
+	 */
+	private static List<Card> remove() {
+		
+		private static List<Card> wagonCards;
+		
+		for(int i = 0; i < (Card.values()).length; ++i) {
+			if((Card.values())[i] != null) {
+				wagonCards.add((Card.values())[i]);
+			}
+		}
+		return wagonCards;
+	}
+	
+	
+	/**
+	 * Constructor initializing the color of the wagon cards. 
+	 * @param colorName
+	 */
+	private Card(String color) {
+		colorName = color;
+	}
+	
+	
+	/**
+	 * 
+	 * @param color : the card's color.
+	 * @return the type of the corresponding wagon card.
+	 */
+	public static Card of(Color color) {
+		
+		return color.colorName();
+	}
+	
+	/**
+	 * 
+	 * @return the color of the card's type if it's a wagon card. null if not.
+	 */
 	public Color color() {
-		if(this)
+		
 	}
 
 	
