8e2e2b816352ebd19d0d78de5149d2c0e2c13b18
yasmineben510 <yasmin.benrahhal@epfl.chh>
2021-03-01 17:03:25 +0100

    trail

--- src/ch/epfl/tchu/game/Trail.java
+++ src/ch/epfl/tchu/game/Trail.java
@@ -1,5 +1,6 @@
 package ch.epfl.tchu.game;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,12 +13,80 @@ import java.util.List;
 
 public final class Trail { // Note : rendre la classe immuable
 	
+	private final int lenght;
+	private final Station station1;
+	private final Station station2;
+	private final List<Route> routes;
+	
+	
+	/**
+	 * private constructor for a Trail
+	 * 
+	 * @param station1 (Station) the departure's Station of the Trail
+	 * @param station2 (Station) the arrival's Station of the Trail
+	 * @param routes (List<Route>) all the roads taken by the Trail 
+	 */
+	private Trail(Station station1, Station station2, List<Route> routes) {
+		
+		this.lenght = computeLenght();
+		this.station1 = station1;
+		this.station2 = station2;
+		this.routes = routes;
+	}
+
+	private int computeLenght() {
+		if (routes.equals(null) || routes.size()==0) {
+			return 0;
+		}
+		
+		int l=0;
+		for (Route r : routes){
+			l += r.length();
+		}
+		return l;
+	}
+	
+	
+	
 	/**
 	 * 
-	 * @param (List<Route>) routes :
+	 * @param routes (List<Route>) :
 	 * @return (Trail) the longest path in the given list of routes
 	 */
-	static Trail longest(List<Route> routes) {
+	public static Trail longest(List<Route> routes) {
+		if (routes.equals(null) || routes.size() == 0) {
+			return new Trail(null, null,null);
+		}
+		
+		
+		List<Trail> trails = List.of(); //unomidifiable?
+		for (Route r : routes) {
+			trails.add(new Trail(r.station1(),r.station2(),List.of(r)));
+		}
+		
+		while (trails.size()!=0) {
+			
+			List<Trail> cs = List.of();
+			for (Trail t : trails) {
+				
+				List<Route> road = routes;
+				road.removeAll(t.routes); // enleve routes deja presentes dans "t"
+				for ( Route r : road) { 
+					if (!(r.station1().equals(t.station2)) && !(r.station2().equals(t.station2))) { // enleve routes ne  pouvant pas prolonger t
+						road.remove(r);
+					}
+				}
+				
+				for (Route r : road ) {
+					List<Route> newRoad = t.routes;
+					newRoad.add(r); // route t prolongee de r
+					cs.add(new Trail(t.station1,r.station2(),newRoad)); // nouveau chemin prolonge
+				}
+			}
+			
+			trails = cs;
+		}
+		
 		return null;
 	}
 	
@@ -25,21 +94,27 @@ public final class Trail { // Note : rendre la classe immuable
 	 * @return (int) the trail's length
 	 */
 	public int length() {
-		return 0;
+		return this.lenght;
 	}
 	
 	/**
 	 * @return (Station) the first station of the trail. null iff the trail's length is zero
 	 */
 	public Station station1() {
-		return null;
+		if (lenght == 0) {
+			return null;
+		}
+		return this.station1;
 	}
 	
 	/**
 	 * @return (Station) the last station of the trail. null iff the trail's length is zero
 	 */
     public Station station2() {
-		return null;
+    	if (lenght == 0) {
+			return null;
+		}
+    	return this.station2;
 	}
     
     @Override
