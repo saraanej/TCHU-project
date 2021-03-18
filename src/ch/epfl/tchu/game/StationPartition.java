60a647318374f09f99554272ce9651ed13009ce2
yasmineben510 <yasmin.benrahhal@epfl.chh>
2021-03-18 01:57:22 +0100

    stp

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -20,47 +20,34 @@ public final class StationPartition implements StationConnectivity{
     /**
      *
      * private constructor
-     *
      * @param links (int[]) : list containing the links linking each element to the representative of their subset
      */
     private StationPartition(int[] links){ // RENDRE PRIVATE
-
         int[] newTab = (int[]) links.clone();
-
         this.links = newTab;
-
     }
 
 
     /**
      * verifies if the two given stations are connected
-     *
      * @param s1 (Station) the first given station
      * @param s2 (Station) the second given station
-     *
      * @return true if the two stations are connected. false if not.
      */
     @Override
     public boolean connected(Station s1, Station s2) {
-
         if (s1.id() >= links.length || s2.id() >= links.length){
-
             return s1.id() == s2.id() ? true : false; // comparer les id
-
         }else{
-
             return (links[s1.id()] == links[s2.id()]) ? true : false; // comparer les représentants
-
         }
     }
 
 
     /**
      * The station partition builder
-     *
      */
     public final static class Builder{
-
         private int stations[];
 
         public int[] getStations() { // A SUPPRIMER
@@ -69,39 +56,30 @@ public final class StationPartition implements StationConnectivity{
 
         /**
          * default constructor
-         *
          * @param stationCount (int) : the identity of the station partition
          * @throws IllegalArgumentException
          *             if the stationCount is a negative number
          */
         public Builder(int stationCount){
-
             Preconditions.checkArgument(stationCount >= 0);
-
             stations = new int[stationCount];
-
-            for(int i = 0; i < stationCount; i++ ){
+            for(int i = 0; i < stationCount; ++i ){
                 stations[i] = i;
             }
-
         }
 
         /**
          *
          * joins the subsets containing the two stations by electing one of the two representatives
          * as representative of the joined subset
-         *
          * @param s1 (Station) : the first given station
          * @param s2 (Station) : the second given station
          * @return (Builder) the builder this
          */
         public Builder connect(Station s1, Station s2){
-
             int repS1 = representative(s1.id());
             int repS2 = representative(s2.id());
-
             stations[repS2] = repS1;
-
             return this;
         }
 
@@ -110,17 +88,12 @@ public final class StationPartition implements StationConnectivity{
          * @return (StationPartition) the station partition corresponding to the profound partition that is being created
          */
         public StationPartition build(){
-
             for(int i = 0; i < stations.length; i++){
-
                 if(stations[i] != representative(i)){
-
                     stations[i] = representative(i);
                 }
             }
-
             StationPartition flattened = new StationPartition(stations);
-
             return flattened;
         }
 
@@ -130,22 +103,15 @@ public final class StationPartition implements StationConnectivity{
          * @return the identification number of the representative of the subset containing the station
          */
         private int representative(int stationId){
-
             int representative = stations[stationId];
             int index = stationId;
-
             if(index == representative){
-
                 return stationId;
-
             } else {
-
                 do{
                     index = representative;
                     representative = stations[index];
-
                 } while(representative != index);
-
                 return representative;
             }
         }
