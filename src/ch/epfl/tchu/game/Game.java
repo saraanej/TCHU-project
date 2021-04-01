84c42840be4c022c5b89480a3a3b00defc1c5555
saraanej <sara.anejjar@epfl.ch>
2021-04-01 14:11:32 +0200

    J

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -31,7 +31,6 @@ public class Game {
         Preconditions.checkArgument(players.size() == 2);
         Preconditions.checkArgument(playerNames.size() == 2);
 
-
         // PROBLEME LE GAMESTATE CHANGE JAMS DU COUP LE CURRENTPLAYER NON PLUS
 
         GameState gameState = GameState.initial(tickets,rng);
@@ -39,8 +38,8 @@ public class Game {
         Player player1 = players.get(PlayerId.PLAYER_1);
         Player player2 = players.get(PlayerId.PLAYER_2);
 
-        Info infoPlayer1 = new Info(playerNames.get(PlayerId.PLAYER_1);
-        Info infoPlayer2 = new Info(playerNames.get(PlayerId.PLAYER_2);
+        Info infoPlayer1 = new Info(playerNames.get(PlayerId.PLAYER_1));
+        Info infoPlayer2 = new Info(playerNames.get(PlayerId.PLAYER_2));
 
         Player currentPlayer = players.get(gameState.currentPlayerId());
         Player otherPlayer = players.get(gameState.currentPlayerId().next());
@@ -51,28 +50,25 @@ public class Game {
 
         //DEBUT DE PARTIE
         players.forEach((id,player) -> { player.initPlayers(id,playerNames); });
-        currentPlayer.receiveInfo(infoCurrentPlayer.willPlayFirst());
 
-//Choix de tickets (partie a ameliorer en lambda du coup)
-        //player 1 chooses tickets
+        players.forEach((id,player) -> { player.receiveInfo(infoCurrentPlayer.willPlayFirst()); });
+
         SortedBag<Ticket> initialTickets = gameState.topTickets(5);
         gameState = gameState.withoutTopTickets(5);
         player1.setInitialTicketChoice(initialTickets);
-        SortedBag<Ticket> chosenTickets = player1.chooseInitialTickets();
-        gameState = gameState.withInitiallyChosenTickets(PlayerId.PLAYER_1, chosenTickets);
 
-        //player 2 chooses tickets
         initialTickets = gameState.topTickets(5);
         gameState = gameState.withoutTopTickets(5);
         player2.setInitialTicketChoice(initialTickets);
-        chosenTickets = player2.chooseInitialTickets();
-        gameState = gameState.withInitiallyChosenTickets(PlayerId.PLAYER_2, chosenTickets);
 
-        //players.forEach((id,player) -> { player.setInitialTicketChoice(tickets); });
-        //players.forEach((id,player) -> { player.chooseInitialTickets(); });
-        
-        currentPlayer.receiveInfo(infoCurrentPlayer.keptTickets(currentPlayer.chooseTickets(tickets).size())); //ARGUMENT tickets est faux
-        otherPlayer.receiveInfo(infoOtherPlayer.keptTickets(otherPlayer.chooseTickets(tickets).size()));
+        SortedBag<Ticket> chosenTickets1 = player1.chooseInitialTickets();
+        gameState = gameState.withInitiallyChosenTickets(PlayerId.PLAYER_1, chosenTickets1);
+
+        SortedBag<Ticket> chosenTickets2 = player2.chooseInitialTickets();
+        gameState = gameState.withInitiallyChosenTickets(PlayerId.PLAYER_2, chosenTickets2);
+
+        player1.receiveInfo(infoPlayer1.keptTickets(chosenTickets1.size()));
+        player2.receiveInfo(infoPlayer2.keptTickets(chosenTickets2.size()));
 
 
         // MILIEU DE PARTIE
@@ -87,10 +83,13 @@ public class Game {
                     currentPlayer.drawSlot();
                     currentPlayer.drawSlot();
                 case CLAIM_ROUTE:
-                    currentPlayer.claimedRoute();
-                    currentPlayer.initialClaimCards();
-                    if (currentPlayer.claimedRoute().equals(Route.Level.UNDERGROUND)
-
+                    Route route = currentPlayer.claimedRoute();
+                    SortedBag<Card> claimCards = currentPlayer.initialClaimCards();
+                    int nbAdditionalCards = route.additionalClaimCardsCount(claimCards, topCards(gameState,3));
+                    if (route.equals(Route.Level.UNDERGROUND)
+                            && nbAdditionalCards >= 1
+                            && gameState.currentPlayerState().possibleAdditionalCards(nbAdditionalCards, claimCards,topCards(gameState,3)).contains(gameState.currentPlayerState().cards())
+                             // A MODIFIER IL FAUT VERIFIER QUE LES CARTES DU JOUEUR CONTIENNENT UNE DES COMPO DE POSSIBLEADDITIONALCARDS
                     ) {
                         //currentPlayer.chooseAdditionalCards();
                     }
@@ -113,13 +112,13 @@ public class Game {
      * @param n (int) : number of topCards wanted with n excluded
      * @return (SortedBag<Card>) the first n cards in the deck
      */
-    private SortedBag<Card> topCards(GameState gameState, int n){
+    private static SortedBag<Card> topCards(GameState gameState, int n){
         List<Card> topCards = new ArrayList<>();
         for(int i = 0; i < n; ++i){
             topCards.add(gameState.topCard());
         }
         return SortedBag.of(topCards);
     }
-
+    
 
 }
