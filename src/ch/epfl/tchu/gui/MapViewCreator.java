83ebc675b1d81af74e9cae54a4142e11147cf2ef
yasmineben510 <yasmin.benrahhal@epfl.chh>
2021-04-30 00:22:31 +0000

    g

---
+++ src/ch/epfl/tchu/gui/MapViewCreator.java
@@ -0,0 +1,12 @@
+package ch.epfl.tchu.gui;
+
+//non instanciable et package private
+class MapViewCreator {
+    private MapViewCreator(){}
+
+//    public void createMapView(ObservableGameState observable, ObjectProperty<ClaimRouteHandler> routeHandler, CardChooser cardChooser){
+//
+//    }
+
+    
+}
---
+++ test/ch/epfl/tchu/gui/ObservableGameState.java
@@ -0,0 +1,5 @@
+package ch.epfl.tchu.gui;
+
+//instanciable et jsp si package private ou public
+public class ObservableGameState {
+}
