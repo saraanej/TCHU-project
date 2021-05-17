9a815c11a122d68fcd66a89ace8dc580b4d6c6ac
saraanej <sara.anejjar@epfl.ch>
2021-05-17 16:37:13 +0100

    j

--- src/ch/epfl/tchu/gui/ServerMain.java
+++ src/ch/epfl/tchu/gui/ServerMain.java
@@ -1,7 +1,15 @@
 package ch.epfl.tchu.gui;
 
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.ServerSocket;
+import java.util.Map;
+import java.util.Random;
 
 public class ServerMain extends Application {
 
@@ -14,11 +22,28 @@ public class ServerMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        String player1;
-        String player2;
-        if(getParameters().getRaw().size() == 2) {
+        String player1, player2;
+        player1 = (getParameters().getRaw().size() >= 1) ? getParameters().getRaw().get(0) : PLAYER_1_DEFAULT;
+        player2 = (getParameters().getRaw().size() >= 2) ? getParameters().getRaw().get(1) : PLAYER_2_DEFAULT;
 
+        try {
+            ServerSocket socket = new ServerSocket(5108);
+            socket.accept();
+        } catch (IOException e){
+            throw new UncheckedIOException(e);
         }
+
+        Random rng = new Random();
+        SortedBag<Ticket> tickets = SortedBag.of(ChMap.tickets());
+        Map<PlayerId, String> names =
+                Map.of(PlayerId.PLAYER_1, player1, PlayerId.PLAYER_2, player2);
+        Map<PlayerId, Player> players =
+                Map.of(PlayerId.PLAYER_1, new GraphicalPlayerAdapter(),
+                       PlayerId.PLAYER_2, new );
+
+
+        new Thread(() -> Game.play(players, names, tickets, rng))
+                .start();
 
 
     }
